dd96968130343fae9dc0d5a32457640e3775766d
alamovAsadbek <alamovasad@gmail.com>
2024-12-12 17:01:40 +0500

    change

--- app_basket/views.py
+++ app_basket/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from app_common.premissions import IsOwnerOrReadOnly
+from .serializers import BasketSerializer
+from .models import BasketModel
+
 
 class BasketView(View):
-    pass
+    serializer_class = BasketSerializer
+    queryset = BasketModel.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
