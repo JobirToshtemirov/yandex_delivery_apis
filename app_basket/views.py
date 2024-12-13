08f91d210b6f218fd8446d302d1333fe3b8354cc
alamovAsadbek <alamovasad@gmail.com>
2024-12-13 14:21:40 +0500

    change

--- app_basket/views.py
+++ app_basket/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.response import Response
+from rest_framework import status
 from app_common.premissions import IsOwnerOrReadOnly
 from .serializers import BasketSerializer
 from .models import BasketModel
@@ -11,3 +12,22 @@ class BasketView(View):
     serializer_class = BasketSerializer
     queryset = BasketModel.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+    def get(self, request, *args, **kwargs):
+        basket = self.queryset.filter(user=request.user)
+        serializer = self.serializer_class(basket, many=True)
+        response = {
+            'success': True,
+            'data': serializer.data,
+        }
+        return Response(response, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, user=request.user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {
+            'success': True,
+            'data': serializer.data,
+        }
+        return Response(response, status=status.HTTP_201_CREATED)
