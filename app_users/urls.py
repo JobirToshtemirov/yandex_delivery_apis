048bf467c3cb7b14b14fa462462382f4dca1140e
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-15 20:00:24 +0500

    login fixed

--- app_users/urls.py
+++ app_users/urls.py
@@ -6,5 +6,6 @@ from app_users import views as user_views
 urlpatterns = [
     # auth urls
     path('auth/login/', user_views.LoginView.as_view(), name='login'),
+    path('auth/login-with-username/', user_views.LoginWithUsernameView.as_view(), name='login_with_username'),
     path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
 ]
